e84dea515249cd082e36295273156361b8eea97b
Charlesson Mendes Pereira <charlessonmendes@gmail.com>
2022-04-08 13:25:54 -0300

    Adição de mais alguns elementos da interface
    Pequena alteração nos elementos referentes ao ferreiro e a inserção de arquivos e imagens para representar os guerreiros na simulação.

--- Principal.java
+++ Principal.java
@@ -32,10 +32,16 @@ public class Principal extends Application { //Inicio da classe principal
   private File fileImgFundo;
   private File fileImgFerreiro1;
   private File fileImgFerreiro2;
+  private File fileImgGuerreiro1;
+  private File fileImgGuerreiro2;
+  private File fileImgGuerreiro3;
 
   private ImageView imgFundo;
   private ImageView imgFerreiro1;
   private ImageView imgFerreiro2;
+  private ImageView imgGuerreiro1;
+  private ImageView imgGuerreiro2;
+  private ImageView imgGuerreiro3;
 
   private Button btn;
   //Fim da declaracao de atributos
@@ -76,10 +82,25 @@ public class Principal extends Application { //Inicio da classe principal
     imgFerreiro1 = new ImageView(fileImgFerreiro1.getPath()); //Instanciando imgFerreiro
     imgFerreiro1.setY(330); //Setando a coordenada y do atributo imgFerreiro1
 
-    fileImgFerreiro2 = new File("/imagens/ferreiro2.png");
-    imgFerreiro2 = new ImageView(fileImgFerreiro2.getPath());
-    imgFerreiro2.setY(255);
-    imgFerreiro2.setVisible(false);
+    fileImgFerreiro2 = new File("/imagens/ferreiro2.png"); //Instanciando atributo fileImgFerreiro2
+    imgFerreiro2 = new ImageView(fileImgFerreiro2.getPath()); //Instanciando atributo imgFerreiro2
+    imgFerreiro2.setY(255); //Setando a coordenada y do atributo imgFerreiro2
+    imgFerreiro2.setVisible(false); //Desabilitando a visibilidade do atributo imgFerreiro2
+
+    fileImgGuerreiro1 = new File("/imagens/guerreiro1.png"); //Instanciando atributo fileImgGuerreiro1
+    imgGuerreiro1 = new ImageView(fileImgGuerreiro1.getPath()); //Instanciando atributo imgGuerreiro1
+    imgGuerreiro1.setX(800);
+    imgGuerreiro1.setY(400);
+    
+    fileImgGuerreiro2 = new File("/imagens/guerreiro2.png"); //Instanciando atributo fileImgGuerreiro2
+    imgGuerreiro2 = new ImageView(fileImgGuerreiro2.getPath());
+    imgGuerreiro2.setX(600);
+    imgGuerreiro2.setY(400);
+
+    fileImgGuerreiro3 = new File("/imagens/guerreiro3.png"); //Instanciando atributo fileImgGuerreiro2
+    imgGuerreiro3 = new ImageView(fileImgGuerreiro3.getPath());
+    imgGuerreiro3.setX(400);
+    imgGuerreiro3.setY(400);
 
     btn = new Button("Apertar");
     btn.setLayoutX(300);
@@ -98,7 +119,7 @@ public class Principal extends Application { //Inicio da classe principal
     });
 
 
-    root = new Group(imgFundo, imgFerreiro1, imgFerreiro2, btn); //Instanciando atributo root
+    root = new Group(imgFundo, imgFerreiro1, imgFerreiro2, btn, imgGuerreiro1, imgGuerreiro2, imgGuerreiro3); //Instanciando atributo root
     scene = new Scene(root, 1000, 690, Color.BLACK); //Instanciando atributo scene
   } //Fim do metodo inicializar
 
